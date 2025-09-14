74e32ce3eee4dc24fb4ab66900e0e6640289cb63
Yaroslav4tez <yaroslav4tez@gmail.com>
2025-09-14 20:31:55 +0200

    fdfsl

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'yaroslav4tez.github.io/home_work_27.1/'
+  base: '/home_work_27.1/'
 })
